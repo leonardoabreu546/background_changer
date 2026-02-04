bf1bcd350c830a14351723b71da2c743ad23332b
Leonardo <lcat140507@gmail.com>
2026-02-04 21:26:24 +0000

    simplificar javascript

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-window.addEventListener('load', () => {
-    const buttons = document.querySelectorAll('.button');
-    const container = document.querySelector('.container');
-
-    if (!container) {
-        console.error('Container não encontrado!');
-        return;
-    }
-
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            const style = window.getComputedStyle(button);
-            const bgColor = style.getPropertyValue('background-color');
-
-            if (!bgColor) {
-                console.error('Não foi possível ler a cor do botão:', button);
-                return;
-            }
-
-            container.style.backgroundColor = bgColor;
-        });
-    });
-});
+function mudarCor(cor) {
+    document.querySelector('.container').style.backgroundColor = cor;
+}
