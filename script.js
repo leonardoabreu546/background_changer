7bcff3eee7633f3c90187d266fd93ec08a86b055
Leonardo <lcat140507@gmail.com>
2026-02-06 22:18:00 +0000

    aplicar javascript ao botao

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+    const cores = ['orange', '#f5c2a0', 'lightgreen', 'green', 'pink'];
+
+    let corAtual = '#E6E6E6';
+
     function mudarCor(cor) {
         document.querySelector('.container').style.backgroundColor = cor
 
         document.getElementById('corAtual').innerText =
             'A cor usada é: ' + cor;
+        
+        corAtual = cor;
     }
+
+    function mudarCorAleatorio() {
+        let corSorteada;
+
+        do {
+            const indice = Math.floor(Math.random() * cores.length);
+            corSorteada = cores[indice];
+        } while (corSorteada === corAtual);
+            mudarCor(corSorteada);
+    }
+
+
