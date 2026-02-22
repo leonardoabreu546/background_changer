3fad9c793d502adca426a19e5220d2d88baf7fdd
Leonardo <lcat140507@gmail.com>
2026-02-22 17:02:58 +0000

    corrigir função mudar cor temporária

--- script.js
+++ script.js
@@ -38,51 +38,53 @@ function mudarCorAleatorio() {
     corAtual = corSorteada;
 }
 
-    function mudarCorTemporaria() {
-        mudarCor('black');
-        document.querySelector('.container').style.color = 'white';
+function mudarCorTemporaria() {
+    mudarCorClasse('bg-black', 'black');
+    document.querySelector('.container').style.color = 'white';
+    document.querySelector('.hora').style.color = 'white';
 
-        setTimeout(function() {
-            mudarCor('white');
-            document.querySelector('.container').style.color = 'black';
-        }, 3000);
+    setTimeout(function () {
+        mudarCorClasse('bg-white', 'white');
+        document.querySelector('.container').style.color = 'black';
+        document.querySelector('.hora').style.color = 'black';
+    }, 3000);
+}
+
+function mostrarHora() {
+    const agora = new Date();
+
+    document.getElementById('hora').innerText =
+        'Data: ' + agora.toLocaleString();
+    return agora;
+}
+
+function apagarTudo() {
+    document.body.innerHTML = '';
+}
+
+function guardarNome() {
+    const nome = document.getElementById('nome').value;
+    document.getElementById('mensagem').innerText = mostrarSaudacao() + ' ' + nome + '!';
+}
+
+function mostrarSaudacao() {
+    const agora = mostrarHora();
+    const hora = agora.getHours();
+
+    let saudacao;
+    if (hora < 12) {
+        saudacao = 'Bom dia';
+    } else if (hora < 18) {
+        saudacao = 'Boa tarde';
+    } else {
+        saudacao = 'Boa noite';
     }
 
-    function mostrarHora() {
-        const agora = new Date();
+    return saudacao;
+}
 
-        document.getElementById('hora').innerText =
-            'Data: ' + agora.toLocaleString();
-        return agora;
-    }
-
-    function apagarTudo() {
-        document.body.innerHTML = '';
-    }
-
-    function guardarNome() {
-        const nome = document.getElementById('nome').value;
-        document.getElementById('mensagem').innerText = mostrarSaudacao() + ' ' + nome + '!';
-    }
-
-    function mostrarSaudacao() {
-        const agora = mostrarHora();
-        const hora = agora.getHours();
-
-        let saudacao;
-        if (hora < 12) {
-            saudacao = 'Bom dia';
-        } else if (hora < 18) {
-            saudacao = 'Boa tarde';
-        } else {
-            saudacao = 'Boa noite';
-        }
-
-        return saudacao;
-    }
-
-    function escolherCor() {
-        const cor = document.getElementById('cor').value;
-        document.querySelector('.container').style.backgroundColor = cor;
-        mudarCor(cor);
+function escolherCor() {
+    const cor = document.getElementById('cor').value;
+    document.querySelector('.container').style.backgroundColor = cor;
+    mudarCor(cor);
 }
